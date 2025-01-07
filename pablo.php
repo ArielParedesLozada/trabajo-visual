c42b4b7b1cd0869a157315f8003ea76256e394f0
Pablo Montero <pmontero4909@uta.edu.ec>
2025-01-07 14:36:38 -0500

    pablo

---
+++ pablo.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pablo
+{
+    public function ciclo()
+    {
+        for ($i = 0; $i < 5; $i++) { 
+            for ($j = 0; $j < 5; $j++) { 
+                echo '*';
+            }
+            echo '<br>'; 
+        }
+    }
+}
+
+$pablo = new Pablo(); 
+
+$pablo->ciclo();
