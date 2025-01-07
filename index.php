bdddc0ddc99585468581998be3ada7b4be0a6738
leonel <lozadaleonel15@gmail.com>
2025-01-07 14:34:20 -0500

    Se agrego los enlaces a las paginas .php

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trabajo de computación visual</title>
+    <title>Trabajo de Computación Visual</title>
 </head>
 <body>
+    <h1>Trabajo de Computación Visual</h1>
     <p>Este es un texto de ejemplo</p>
+
+    <h2>Enlaces a las páginas</h2>
+    <ul>
+        <li><a href="Diego.php">Diego</a></li>
+        <li><a href="Elkin.php">Elkin</a></li>
+        <li><a href="Leonel.php">Leonel</a></li>
+        <li><a href="Paredes.php">Paredes</a></li>
+        <li><a href="Sebastian.php">Paredes</a></li>
+        <li><a href="Pablo.php">Paredes</a></li>
+    </ul>
 </body>
 </html>
